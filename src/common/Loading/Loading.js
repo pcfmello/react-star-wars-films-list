bf2edef984d790acdeec1fce5a566e0e49f05347
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-02-28 12:54:45 -0300

    Create loading component - It was created showing to user something is loading. - It is a common component so was included in the component library.

---
+++ src/common/Loading/Loading.js
@@ -0,0 +1,32 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+import CircularProgress from "@material-ui/core/CircularProgress";
+import { withStyles } from "@material-ui/core/styles";
+
+const styles = {
+  root: {
+    height: 300,
+    padding: 24,
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center"
+  },
+  circularProgress: {
+    color: "gray"
+  }
+};
+
+const Loading = ({ classes, ...props }) => (
+  <div {...props} className={classes.root}>
+    <CircularProgress {...props} className={classes.circularProgress} />
+  </div>
+);
+Loading.propTypes = {
+  /** It's the size of loading bar */
+  size: PropTypes.number,
+  /** @ignore */
+  classes: PropTypes.object.isRequired
+};
+
+export default withStyles(styles)(Loading);
---
+++ src/common/Loading/Loading.md
@@ -0,0 +1,9 @@
+Sample of Loading Bar
+
+```js
+<div style={{ display: "flex" }}>
+  <Loading size={300} style={{ color: "gray" }} />
+  <Loading size={100} style={{ color: "blue" }} />
+  <Loading size={150} style={{ color: "red" }} />
+</div>
+```
