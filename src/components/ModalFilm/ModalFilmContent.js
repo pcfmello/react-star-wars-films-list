64d8ebf0d842598477adb9fada56f4f360270f1f
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-03-04 04:53:33 -0300

    Refactoring and include ErrorPage component

--- src/components/ModalFilm/ModalFilmContent.js
+++ src/components/ModalFilm/ModalFilmContent.js
@@ -3,9 +3,10 @@ import PropTypes from "prop-types";
 import moment from "moment";
 import { Grid } from "@material-ui/core";
 import FilmService from "../../services/FilmsService";
-import injectSheet from "react-jss";
+import { withStyles } from "@material-ui/core/styles";
 
 import Loading from "../../common/Loading/Loading";
+import ErrorPage from "../../common/Error/Error";
 import FilmDetail from "./ModalFilmDetail";
 
 const styles = {
@@ -29,6 +30,7 @@ const ModalFilmContent = ({ film, classes }) => {
   const [vehicles, setVehicles] = useState([]);
   const [species, setSpecies] = useState([]);
   const [isLoading, setLoading] = useState(false);
+  const [isError, setError] = useState(false);
 
   useEffect(() => {
     setLoading(true);
@@ -37,6 +39,7 @@ const ModalFilmContent = ({ film, classes }) => {
 
   const getLists = async () => {
     try {
+      setError(false);
       const respCharacters = await FilmService.getListOf(film.characters);
       const respPlanets = await FilmService.getListOf(film.planets);
       const respStarships = await FilmService.getListOf(film.starships);
@@ -48,10 +51,9 @@ const ModalFilmContent = ({ film, classes }) => {
       setStarships(respStarships.map(starship => starship.data));
       setVehicles(respVehicles.map(vehicle => vehicle.data));
       setSpecies(respSpecies.map(specie => specie.data));
-
-      setLoading(false);
     } catch {
-      // setError(true);
+      setError(true);
+    } finally {
       setLoading(false);
     }
   };
@@ -67,45 +69,55 @@ const ModalFilmContent = ({ film, classes }) => {
 
   return (
     <React.Fragment>
+      {isError && (
+        <ErrorPage
+          iconName="error_outline"
+          message="There was an error to loading the film details"
+        />
+      )}
+      {/* Applied inline style to force change it styles */}
       {isLoading && <Loading style={styles.loading} size={150} />}
-      {!isLoading && (
+      {!isError && !isLoading && (
         <Grid container className={classes.root} spacing={16}>
-          <Grid item md={4}>
+          <Grid item xs={6} md={4}>
             <FilmDetail label="Title" description={film.title} />
           </Grid>
-          <Grid item md={4}>
+          <Grid item xs={6} md={4}>
             <FilmDetail label="Episode" description={film.episode_id.toString()} />
           </Grid>
-          <Grid item md={4}>
-            <FilmDetail label="Release date" description={moment(film.release_date).format("LL")} />
-          </Grid>
-          <Grid item md={4}>
+          <Grid item xs={6} md={4}>
             <FilmDetail label="Director" description={film.director} />
           </Grid>
-          <Grid item md={8}>
+          <Grid item xs={6} md={4}>
             <FilmDetail label="Producer(s)" description={film.producer} />
           </Grid>
-          <Grid item md={12}>
+          <Grid item xs={6} md={4}>
+            <FilmDetail label="Release date" description={moment(film.release_date).format("LL")} />
+          </Grid>
+          <Grid item xs={6} md={4}>
+            <FilmDetail label="Last edition" description={moment(film.edited).format("LL")} />
+          </Grid>
+          <Grid item xs={12}>
             <FilmDetail label="Synopsis" description={film.opening_crawl} />
           </Grid>
 
-          <Grid item md={12}>
+          <Grid item xs={12}>
             <FilmDetail label="Characters" description={formattedNamesList(characters)} />
           </Grid>
 
-          <Grid item md={12}>
+          <Grid item xs={12}>
             <FilmDetail label="Planets" description={formattedNamesList(planets)} />
           </Grid>
 
-          <Grid item md={12}>
+          <Grid item xs={12}>
             <FilmDetail label="Starships" description={formattedNamesList(starships)} />
           </Grid>
 
-          <Grid item md={12}>
+          <Grid item xs={12}>
             <FilmDetail label="Vehicles" description={formattedNamesList(vehicles)} />
           </Grid>
 
-          <Grid item md={12}>
+          <Grid item xs={12}>
             <FilmDetail label="Species" description={formattedNamesList(species)} />
           </Grid>
         </Grid>
@@ -119,4 +131,4 @@ ModalFilmContent.propTypes = {
   classes: PropTypes.object
 };
 
-export default injectSheet(styles)(ModalFilmContent);
+export default withStyles(styles)(ModalFilmContent);
