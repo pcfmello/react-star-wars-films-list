b1b3cf0decf0ca356cf4a52f36696b3a4cf84b9e
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-03-03 22:37:34 -0300

    Setted body height 100% , deleted momentjs locale

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import React, { Component } from "react";
 import "typeface-roboto";
-import "moment/locale/pt-br";
 import Header from "./components/Header/Header";
 import Main from "./components/Main/Main";
 
 class App extends Component {
   render() {
     return (
-      <div className="App" style={{ backgroundColor: "black" }}>
+      <div
+        className="App"
+        style={{ backgroundColor: "black", height: "100vh", overflow: "scroll" }}
+      >
         <Header />
         <Main />
       </div>
