7ff276ab23451bac45ea15c090689307feced3f5
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-03-04 04:48:43 -0300

    Transformed file in a list film component

--- src/components/Films/Films.js
+++ src/components/Films/Films.js
@@ -1,62 +1,133 @@
-import React, { useState, useEffect } from "react";
-import ModalFilm from "../ModalFilm/ModalFilm";
-import FilmService from "../../services/FilmsService";
+import React from "react";
+import PropTypes from "prop-types";
+import cn from "classnames";
+import moment from "moment";
+import { Hidden, Grid, Button } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 
-import List from "../List/List";
+import Loading from "../../common/Loading/Loading";
+import ErrorPage from "../../common/Error/Error";
 
-const styles = theme => ({
-  root: {
-    width: "100%",
-    marginTop: theme.spacing.unit * 3,
-    overflowX: "auto"
+const styles = {
+  tableRow: {
+    display: "flex",
+    padding: "12px 24px",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    alignItems: "center",
+    fontSize: "1.5em",
+    position: "relative",
+    color: "gray"
   },
-  table: {
-    minWidth: 700
+  tableRowHeader: {
+    color: "black",
+    backgroundColor: "gray",
+    borderBottom: "1px solid black"
   },
-  tableCellHeader: {
-    fontSize: "1.25em"
-  },
-  tableBodyCell: {
-    fontSize: "1.5em"
-  }
-});
-
-const Films = () => {
-  const [film, setFilm] = useState({});
-  const [films, setFilms] = useState([]);
-  const [isError, setError] = useState(false);
-  const [isOpenModal, setOpenModal] = useState(false);
-
-  useEffect(() => {
-    getFilms();
-  }, []);
-
-  const getFilms = async () => {
-    setError(false);
-    try {
-      const resp = await FilmService.getAllFilms();
-      setFilms(resp.data.results);
-    } catch {
-      setError(true);
+  tableRowHover: {
+    cursor: "pointer",
+    "&:hover": {
+      color: "black",
+      backgroundColor: "gray"
+    },
+    "&:active": {
+      backgroundColor: "rgba(128, 128, 128, 0.5)"
     }
-  };
+  },
+  tableRowMobile: {
+    padding: 8
+  }
+};
 
-  const openModal = film => {
-    setFilm(film);
-    setOpenModal(true);
-  };
-  const closeModal = () => {
-    setFilm({});
-    setOpenModal(false);
-  };
+const Films = ({ openModal, items, isError, classes }) => {
+  const headerRow = () => (
+    <Hidden xsDown>
+      <Grid container className={cn(classes.flex, classes.tableRow, classes.tableRowHeader)}>
+        <Grid item sm={8} md={5} lg={4}>
+          <div className={classes.flex}>TITLE</div>
+        </Grid>
+        <Hidden mdDown>
+          <Grid item lg={2}>
+            <div className={classes.flex}>EPISODE</div>
+          </Grid>
+        </Hidden>
+        <Hidden smDown>
+          <Grid item md={5} lg={4}>
+            <div className={classes.flex}>DIRECTOR</div>
+          </Grid>
+        </Hidden>
+        <Grid item sm={3} md={2} lg={2}>
+          <div className={classes.flex}>RELEASE</div>
+        </Grid>
+      </Grid>
+    </Hidden>
+  );
+
+  const bodyRow = item => (
+    <React.Fragment key={item.url}>
+      <Hidden xsDown>
+        <Grid
+          container
+          key={item.url}
+          className={cn(classes.flex, classes.tableRow, classes.tableRowHover)}
+          onClick={() => openModal(item)}
+        >
+          <Grid item sm={8} md={5} lg={4}>
+            <div className={classes.flex}>{item.title}</div>
+          </Grid>
+          <Hidden mdDown>
+            <Grid item lg={2}>
+              <div className={classes.flex}>{item.episode_id}</div>
+            </Grid>
+          </Hidden>
+          <Hidden smDown>
+            <Grid item md={5} lg={4}>
+              <div className={classes.flex}>{item.director}</div>
+            </Grid>
+          </Hidden>
+          <Grid item sm={3} md={2} lg={2}>
+            <div className={classes.flex}>{moment(item.release_date).format("L")}</div>
+          </Grid>
+        </Grid>
+      </Hidden>
+
+      <Hidden smUp>
+        <div
+          key={item.url}
+          className={cn(
+            classes.flex,
+            classes.tableRow,
+            classes.tableRowHover,
+            classes.tableRowMobile
+          )}
+        >
+          <Button variant="contained" size="large" fullWidth onClick={() => openModal(item)}>
+            {item.title}
+          </Button>
+        </div>
+      </Hidden>
+    </React.Fragment>
+  );
 
   return (
-    <div>
-      <List items={films} {...{ openModal, isError }} />
-      {isOpenModal && <ModalFilm {...{ film }} open={isOpenModal} handleClose={closeModal} />}
+    <div className={cn(classes.root)}>
+      <div>{headerRow()}</div>
+      <div>
+        {isError && (
+          <ErrorPage iconName="error_outline" message="There was an error to loading films list" />
+        )}
+        {(!isError && (items.length && items.map(item => bodyRow(item)))) ||
+          (!isError && !items.length && <Loading size={100} />)}
+      </div>
     </div>
   );
+};
+
+Films.propTypes = {
+  openModal: PropTypes.func.isRequired,
+  items: PropTypes.array.isRequired,
+  isError: PropTypes.bool.isRequired,
+  classes: PropTypes.object.isRequired
 };
 
 export default withStyles(styles)(Films);
