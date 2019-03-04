d43ca9436c04883fcf5a834a29371b9b19e33968
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-03-04 20:00:27 -0300

    Refactoring and Include logo and background image

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -4,6 +4,9 @@ import cn from "classnames";
 import { Hidden } from "@material-ui/core";
 import { withStyles } from "@material-ui/core/styles";
 
+import backgroundHeaderDesktop from "../../assets/images/background_header_desktop.jpeg";
+import logo from "../../assets/images/logo.png";
+
 const styles = {
   root: {
     position: "relative",
@@ -11,10 +14,10 @@ const styles = {
   },
   headerBackground: {
     height: 300,
-    maxHeight: 300,
-    backgroundImage:
-      "url(https://lumiere-a.akamaihd.net/v1/images/winter-13-header-2x_0a965898.jpeg?region=0%2C0%2C2400%2C444)",
+    backgroundImage: `url(${backgroundHeaderDesktop})`,
     backgroundSize: "cover",
+    backgroundBlendMode: "darken",
+    backgroundColor: "rgba(0,0,0,0.5)",
     backgroundPosition: "center",
     display: "flex",
     justifyContent: "center",
@@ -25,28 +28,18 @@ const styles = {
     maxHeight: 130
   },
   headerLogoMobile: {
-    margin: 24
+    margin: "24px 24px 0"
   }
 };
+
+const getLogo = classes => <img alt="Site Logo" src={logo} className={cn(classes)} />;
 
 const Header = ({ classes }) => (
   <header className={classes.root}>
     <Hidden xsDown>
-      <div className={classes.headerBackground}>
-        <img
-          alt="Logotipo do site"
-          srcSet="https://static-mh.content.disney.io/starwars/assets/navigation/sw_logo_stacked-336c62367939.png"
-          className={classes.headerLogo}
-        />
-      </div>
+      <div className={classes.headerBackground}>{getLogo(classes.headerLogo)}</div>
     </Hidden>
-    <Hidden smUp>
-      <img
-        alt="Logotipo do site"
-        srcSet="https://cdn.shopify.com/s/files/1/1405/7728/collections/star_wars_clear.png?v=1494619544"
-        className={cn(classes.headerLogo, classes.headerLogoMobile)}
-      />
-    </Hidden>
+    <Hidden smUp>{getLogo([classes.headerLogo, classes.headerLogoMobile])}</Hidden>
   </header>
 );
 
