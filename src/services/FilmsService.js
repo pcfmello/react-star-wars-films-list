8288566be072c63254f4cee41e25264a72371306
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-03-03 22:43:35 -0300

    Created Service to get lists of film details

--- src/services/FilmsService.js
+++ src/services/FilmsService.js
@@ -1,5 +1,10 @@
 import API from "./api";
+import { Promise } from "es6-promise";
 
 export default {
-  getAllFilms: () => API.get("films")
+  getAllFilms: () => API.get("https://swapi.co/api/films/"),
+  getListOf: urlList =>
+    Promise.all(urlList.map(url => API.get(url))).then(
+      urlList.map((url, index) => `response${index}`)
+    )
 };
