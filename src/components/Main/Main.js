c0f16449e31e3a6e7c6aab21d2e3b77983e22ef7
Paulo Cesar Ferreira de Mello <pcfmello@gmail.com>
2019-03-04 04:52:17 -0300

    Styling component and fix Film import

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -2,22 +2,24 @@ import React from "react";
 import PropTypes from "prop-types";
 import { withStyles } from "@material-ui/core/styles";
 
-import List from "../Films/Films";
+import Films from "../Films";
 
 const styles = theme => ({
   root: {
+    maxWidth: 1366,
     margin: "0 auto",
     padding: 24
   },
-  list: {
-    border: "1px solid gray"
+  films: {
+    border: "1px solid gray",
+    [theme.breakpoints.down("xs")]: { borderRadius: 8 }
   }
 });
 
 const Main = ({ classes }) => (
   <div className={classes.root}>
-    <div className={classes.list}>
-      <List />
+    <div className={classes.films}>
+      <Films />
     </div>
   </div>
 );
